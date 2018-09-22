63b62d83dfca528897c7ef17867c127b7eb35c81
Schild <Philippe.Schild@adesso.de>
2018-09-22 11:42:26 +0200

    added sqlite integration

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Based on https://github.com/maximegris/angular-electron
 # Install
 
 ## Use SQLite with Electron
-1. Run ```npm install --global --production windows-build-tools``` in an admin console.
-2. ```npm install sqlite3 --save```
-3. ```npm install electron-rebuild --save-dev```
+1. Start a terminal as admin
+2. ```$ root> npm install --save-dev electron-rebuild```
+3. ```$ root/server> npm install sqlite --save```
+4. ```$ root/server> npm --add-python-to-path='true' --debug install --global --vs2015 windows-build-tools``` (it's currently not working without the ```--vs2015``` flag)
+5. Restart terminal (so that changed PATH is available)
+6. ```$ root/server> ..\node_modules\.bin\electron-rebuild.cmd -f -w sqlite3```
--- server/main.js
+++ server/main.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('./test/test.db');
+db.serialize(() => {
+    db.run("CREATE TABLE Products (name, barcode, quantity)");
+
+    db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product001', 'xxxxx', 20]);
+    db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product002', 'xxxxx', 40]);
+    db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product003', 'xxxxx', 60]);
+
+    db.each("SELECT * FROM Products", (err, row) => {
+        console.log(row);
+    });
+});
+db.close();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
