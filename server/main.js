eee775c0cca50b4b9c736a15e93137bd69d42b9f
Schild <Philippe.Schild@adesso.de>
2018-09-22 14:16:01 +0200

    database file is now created within a directory, located under the users home directory

--- server/main.js
+++ server/main.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 const sqlite3 = require('sqlite3');
 
-var db = new sqlite3.Database('test.db');
+const appDir = path.join(os.homedir(), '.image-management-tool');
+if (!fs.existsSync(appDir)) {
+    fs.mkdirSync(appDir);
+}
+
+var db = new sqlite3.Database(path.join(appDir, 'image-management-tool.db'));
 db.serialize(() => {
-    db.run("CREATE TABLE Products (name, barcode, quantity)");
+    db.run("CREATE TABLE IF NOT EXISTS Products (name, barcode, quantity)");
 
     db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product001', 'xxxxx', 20]);
     db.run("INSERT INTO Products VALUES (?, ?, ?)", ['product002', 'xxxxx', 40]);
