05fcc02ec78264076f49e75ff177542c3757dc7d
Schild <Philippe.Schild@adesso.de>
2018-09-27 13:55:50 +0200

    using migration API for running migration commands

--- server/migration/1538044562515-TagLabelName.ts
+++ server/migration/1538044562515-TagLabelName.ts
@@ -3,17 +3,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class TagLabelName1538044562515 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "tag" RENAME TO "tmp_tag"`);
-        await queryRunner.query(`CREATE TABLE "tag" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "dateAdded" datetime NOT NULL DEFAULT (datetime('now')))`);
-        await queryRunner.query(`INSERT INTO "tag" (id, name, dateAdded) SELECT id, label, dateAdded FROM "tmp_tag"`);
-        await queryRunner.query(`DROP TABLE "tmp_tag"`);
+        await queryRunner.renameColumn('tag', 'label', 'name');
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "tag" RENAME TO "tmp_tag"`);
-        await queryRunner.query(`CREATE TABLE "tag" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "label" varchar NOT NULL, "dateAdded" datetime NOT NULL DEFAULT (datetime('now')))`);
-        await queryRunner.query(`INSERT INTO "tag" (id, label, dateAdded) SELECT id, name, dateAdded FROM "tmp_tag"`);
-        await queryRunner.query(`DROP TABLE "tmp_tag"`);
+        await queryRunner.renameColumn('tag', 'name', 'label');
     }
 
 }
