67eca5abab27cbe88c4aab130bf81568098c7ba2
Philippe Schild <Philippe.Schild@adesso.de>
2019-01-13 14:52:53 +0100

    removed dependency of rounting-controllers package

--- domain/error/FileSystemError.ts
+++ domain/error/FileSystemError.ts
@@ -1,11 +1,10 @@
-import { HttpError } from 'routing-controllers/http-error/HttpError';
 
-export class FileSystemError extends HttpError {
+export class FileSystemError {
+    public httpCode = 500;
     public errorCode: string;
     public message: string;
 
     constructor(errorCode: string, message: string) {
-        super(500);
         Object.setPrototypeOf(this, FileSystemError.prototype);
         this.errorCode = errorCode;
         this.message = message;
