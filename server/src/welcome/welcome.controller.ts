16e39b0d0a4346b6b6a0bee6e0bdbe1d6048397a
pschild <philippe.schild@googlemail.com>
2019-01-17 09:47:54 +0100

    added missing methods in welcome controller

--- server/src/welcome/welcome.controller.ts
+++ server/src/welcome/welcome.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { WelcomeService } from './welcome.service';
-import { Folder } from '../entity/Folder';
-import { getManager } from 'typeorm';
-import { Person } from '../entity/Person';
-import { Place } from '../entity/Place';
-import { Image } from '../entity/Image';
-import { Tag } from '../entity/Tag';
+import * as path from 'path';
+import * as afs from 'async-file';
+import { post, get } from 'request-promise';
+import { ConfigService } from '../config/config.service';
 
 @Controller('welcome')
 export class WelcomeController {
-    constructor(private readonly welcomeService: WelcomeService) { }
+    constructor(
+        private readonly welcomeService: WelcomeService,
+        private readonly configService: ConfigService
+    ) { }
 
     @Get('greet/:name')
     greet(@Param('name') name: string): string {
@@ -19,5 +20,48 @@ export class WelcomeController {
     @Get('dbtest')
     async test() {
         return this.welcomeService.findAllFolders();
+    }
+
+    @Post('/cropImage')
+    async cropImage(@Body() data: any): Promise<any> {
+        const formData = {
+            file: {
+                value: afs.createReadStream(data.filePath),
+                options: {
+                    filename: path.basename(data.filePath)
+                }
+            },
+            fuzzValue: data.fuzzValue
+        };
+
+        const cropResult = await post(this.configService.get('CROP_SERVICE_URL'), {
+            formData: formData,
+            json: true
+        }).catch(error => {
+            throw new Error(error.message);
+        });
+
+        cropResult.downloadPaths = [];
+        for (const croppedImage of cropResult.croppedImages) {
+            const downloadPath = await this.welcomeService.downloadImage(croppedImage.uri, data.filePath, croppedImage.name);
+            cropResult.downloadPaths.push(downloadPath);
+        }
+
+        const removeUploadFolderResult = await get({
+            uri: this.configService.get('CROP_SERVICE_URL'),
+            qs: {
+                action: 'removeUploadFolder',
+                folderName: cropResult.uploadFolderName
+            },
+            json: true
+        }).catch(error => {
+            throw new Error(error.message);
+        });
+
+        if (!removeUploadFolderResult.success) {
+            throw new Error('Upload folder could not be removed');
+        }
+
+        return cropResult;
     }
 }
--- server/src/welcome/welcome.service.ts
+++ server/src/welcome/welcome.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Folder } from '../entity/Folder';
+import { get } from 'request-promise';
+import * as path from 'path';
+import * as afs from 'async-file';
 
 @Injectable()
 export class WelcomeService {
@@ -19,5 +22,15 @@ export class WelcomeService {
 
     async findAllFolders(): Promise<Folder[]> {
         return this.repository.find();
+    }
+
+    downloadImage(url, directory, name) {
+        return new Promise<string>(async resolve => {
+            await get(url)
+                .pipe(afs.createWriteStream(
+                    path.join(path.dirname(directory), name)
+                ))
+                .on('close', () => resolve(path.join(path.dirname(directory), name)));
+        });
     }
 }
