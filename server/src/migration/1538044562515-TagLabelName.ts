77e0b3312aead9f2f4b5bcd4eea49518bd4d62d2
pschild <philippe.schild@googlemail.com>
2019-01-25 14:50:17 +0100

    disabled migrations

--- server/src/migration/1538044562515-TagLabelName.ts
+++ server/src/migration/1538044562515-TagLabelName.ts
@@ -3,10 +3,12 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class TagLabelName1538044562515 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+        throw new Error(`Migrations NYI`);
         await queryRunner.renameColumn('tag', 'label', 'name');
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        throw new Error(`Migrations NYI`);
         await queryRunner.renameColumn('tag', 'name', 'label');
     }
 
--- server/src/migration/1538046038653-TagNameLabel.ts
+++ server/src/migration/1538046038653-TagNameLabel.ts
@@ -3,10 +3,12 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class TagNameLabel1538046038653 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+        throw new Error(`Migrations NYI`);
         await queryRunner.renameColumn('tag', 'name', 'label');
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        throw new Error(`Migrations NYI`);
         await queryRunner.renameColumn('tag', 'label', 'name');
     }
 
