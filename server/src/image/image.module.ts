a333c6aeb17612d4ccca18994d42e770f93f0ecb
Philippe Schild <Philippe.Schild@adesso.de>
2019-02-01 15:18:59 +0100

    added missing import

--- server/src/image/image.module.ts
+++ server/src/image/image.module.ts
@@ -3,9 +3,13 @@ import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from '../entity/image.entity';
+import { MapperModule } from '../mapper/mapper.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Image])],
+    imports: [
+        TypeOrmModule.forFeature([Image]),
+        MapperModule
+    ],
     controllers: [ImageController],
     providers: [ImageService],
     exports: [ImageService]
