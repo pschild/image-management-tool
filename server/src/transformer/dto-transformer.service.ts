0be37d0c3190ebb24e123c3dbd6b4b4f39bb823d
pschild <philippe.schild@googlemail.com>
2019-03-03 12:21:15 +0100

    renamed generic variables

--- server/src/transformer/dto-transformer.service.ts
+++ server/src/transformer/dto-transformer.service.ts
@@ -5,13 +5,13 @@ import { classToPlain, plainToClass } from 'class-transformer';
 @Injectable()
 export class DtoTransformerService {
 
-    transform<T, E>(value: T, newType: ClassType<E>): E {
+    transform<E, D>(value: E, newType: ClassType<D>): D {
         const plain = classToPlain(value);
         return plainToClass(newType, plain);
     }
 
-    transformList<T, E>(value: T[], newType: ClassType<E>): E[] {
-        const plainList: object[] = value.map((item: T) => classToPlain(item));
+    transformList<E, D>(value: E[], newType: ClassType<D>): D[] {
+        const plainList: object[] = value.map((item: E) => classToPlain(item));
         return plainList.map((item: object) => plainToClass(newType, item));
     }
 
